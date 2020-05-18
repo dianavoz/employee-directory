1712b0f7ce00893b16886f9c728ccac97427ca0e
Diana Vozanu <40432724+dianavoz@users.noreply.github.com>
2020-05-18 23:45:48 +0100

    updated js and css

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,19 @@
-/*============== Variables ===================*/
-const gallery = document.getElementById('gallery');
+const galleryDiv = document.getElementById('gallery');
 const body = document.querySelector('body');
+let employees;
 
 // Create the modal container
 const modalContainer = document.createElement('div');
 modalContainer.className = 'modal-container';
-let employees;
-
 
 /*============== Fetch API ===================*/
-fetch('https://randomuser.me/api/?results=12')
+fetch('https://randomuser.me/api/?results=12&nat=us,au,ca,gb,nz')
     .then(checkStatus)
     .then(data => displayUsers(data.results))
     .catch(error => console.log(error));
 
-/*============== API Error ===================*/
+
+/*============== Error Status===================*/
 function checkStatus(res) {
     if (!res.ok) {
         throw new TypeError("Oops, we haven't got JSON!");
@@ -63,17 +62,18 @@ function displayUsers(data) {
         card.innerHTML = markup;
 
         // Append the card to the gallery
-        gallery.appendChild(card);
+        galleryDiv.appendChild(card);
     });
 }
 
 /*============== Modal Window Markup ===================*/
+
 // Show modal
 function showModal(employee) {
 
     let currentIndex = employee.getAttribute('data-index');
     let currentEmployee = employees[currentIndex];
-    // Create the markup to go inside the modal container
+    // Create the HTML to go inside the modal container
     const markup = `
         <div class="modal">
             <button type="button" id="modal-close-btn" class="modal-close-btn" aria-label="Close"><i class="fa fa-times" aria-hidden="true"></i></button>
@@ -85,7 +85,7 @@ function showModal(employee) {
                 <hr>
                 <p class="modal-text">${currentEmployee.phone}</p>
                 <p class="modal-text">${currentEmployee.location.street}, ${currentEmployee.location.city}, ${currentEmployee.location.state} ${currentEmployee.location.postcode}</p>
-                <p class="modal-text">Birthday: ${formatDateOfBirth(currentEmployee.dob.date)}</p>
+                <p class="modal-text">Birthday: ${formatBirthDate(currentEmployee.dob.date)}</p>
             </div>
         </div>
         <div class="modal-btn-container">
@@ -99,7 +99,8 @@ function showModal(employee) {
     body.appendChild(modalContainer);
 }
 
-function formatDateOfBirth(string) {
+//format date of birth for the current employee
+function formatBirthDate(string) {
     let date = new Date(Date.parse(string));
     let month = date.getMonth() + 1;
     let day = date.getDate();
@@ -108,12 +109,14 @@ function formatDateOfBirth(string) {
     return dob;
 }
 
+// Close the modal
+function closeModal() {
+    body.removeChild(modalContainer);
+}
+
 // When an employee card is clicked
-gallery.addEventListener('click', function(event) {
-    showModal(event.target.closest('.card'));
-    // if (event.target.className !== 'gallery') {
-    //     showModal(event.target.closest('.card'));
-    // }
+galleryDiv.addEventListener('click', function(e) {
+    showModal(e.target.closest('.card'));
 });
 
 // When the modal close button or overlay is clicked
@@ -124,9 +127,74 @@ body.addEventListener('click', function(e) {
     }
 });
 
-// Close the modal
-function closeModal() {
-    body.removeChild(modalContainer);
-}
 
-/*=========================Next Modal======================================*/
+
+/*=========================Toggle======================================*/
+// When the previous or next button is clicked
+body.addEventListener('click', function(e) {
+
+    const target = e.target;
+    if (target.className === 'fa fa-chevron-right' || target.className === 'fa fa-chevron-left' || target.id === 'modal-prev' || target.id === 'modal-next') {
+
+        // Get total cards from the page
+        const card = Array.from(document.querySelectorAll('.card'));
+
+        // Get the current modal
+        let modal;
+        if (target.id === 'modal-prev' || target.id === 'modal-next') {
+            modal = target.parentElement.previousElementSibling; // <div class="modal">
+        } else if (target.className === 'fa fa-chevron-left' || target.className === 'fa fa-chevron-right') {
+            modal = target.parentElement.parentElement.previousElementSibling; // <div class="modal">
+        }
+
+        // Get the name from the modal
+        const modalName = modal.lastElementChild.firstElementChild.nextElementSibling.textContent; // .modal-info-container h3 #name
+
+        // Hide the current modal
+        modal.style.display = 'none';
+
+        // Get the current employee card
+        const currentEmployee = card.filter(card => card.lastElementChild.firstElementChild.textContent === modalName);
+
+        // Get the next employee card
+        let next;
+        let index;
+        if (target.className === 'fa fa-chevron-left' || target.id === 'modal-prev') {
+            next = currentEmployee[0].previousElementSibling;
+            index = card.length - 1;
+        } else if (target.className === 'fa fa-chevron-right' || target.id === 'modal-next') {
+            next = currentEmployee[0].nextElementSibling;
+            index = 0;
+        }
+
+        // Show the modal for the next employee
+        (next) ? showModal(next): showModal(card[index]);
+    }
+});
+
+/*============== Search Input ===================*/
+const input = document.querySelector('.search-input');
+
+input.addEventListener('keyup', function(e) {
+    let employeeCard = document.querySelectorAll('.card');
+    let currentCard = this.value;
+
+    employeeCard.forEach((card, i) => {
+        if (!employeeCard[i].innerText
+            .toUpperCase()
+            .includes(currentCard.toUpperCase())
+        ) {
+            employeeCard[i].style.display = 'none';
+        } else {
+            employeeCard[i].style.display = 'flex';
+        }
+    });
+});
+
+/*============ page refresh ==================*/
+//click on submit to refresh and get the profiles back
+body.addEventListener('click', (e) => {
+    if (e.target.type === 'submit') {
+        window.location.reload();
+    }
+});
